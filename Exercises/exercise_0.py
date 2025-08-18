3d804a31f5d684b69ad42868839c56f6e79296ab
glennart8 <henrikpilback@gmail.com>
2025-08-18 11:51:06 +0200

    Started working on bike_sales_data

--- Exercises/exercise_0.py
+++ Exercises/exercise_0.py
@@ -17,33 +17,35 @@ df_yearly = df.groupby("Year")["Quantity"].sum().reset_index()
 print(df_yearly)
 
 # Bygg en plot
-plt.figure(figsize=(10,6))
-plt.plot(df_yearly["Year"], df_yearly["Quantity"], marker="o") # marker="o" gör att en symbol ritas ut vid varje punkt
-plt.title("Total quantity per year")
-plt.xlabel("Year")
-plt.ylabel("Quantity")
+
+# plt.figure(figsize=(10,6))
+# plt.plot(df_yearly["Year"], df_yearly["Quantity"], marker="o") # marker="o" gör att en symbol ritas ut vid varje punkt
+# plt.title("Total quantity per year")
+# plt.xlabel("Year")
+# plt.ylabel("Quantity")
 # plt.show()
 
 #   c) Draw a line chart of average CO2 emissions for same years that as in b)
 
 df_co2 = df.groupby("Year")["Avg_CO2"].mean().reset_index()
 
-plt.figure(figsize=(10,6)) # (10,6) för att den ska ha en tuple med två värden
-plt.plot(df_co2["Year"], df_co2["Avg_CO2"], marker="o", color="green")
-plt.title("Average CO2 emissions per Year")
-plt.xlabel("Year")
-plt.ylabel("Average CO2")
+# plt.figure(figsize=(10,6)) # (10,6) för att den ska ha en tuple med två värden
+# plt.plot(df_co2["Year"], df_co2["Avg_CO2"], marker="o", color="green")
+# plt.title("Average CO2 emissions per Year")
+# plt.xlabel("Year")
+# plt.ylabel("Average CO2")
 # plt.show()
 
 #   d) Draw a line chart of all years and months for import
 df_import = df.groupby("Year")["Import"].sum().reset_index()
 print(df_import)
-plt.figure(figsize=(10,6)) # (10,6) för att den ska ha en tuple med två värden
-plt.plot(df_import["Year"], df_import["Import"], marker="o")
-plt.title("Yearly imported cars")
-plt.xlabel("Year")
-plt.ylabel("Imported cars")
-plt.show()
+
+# plt.figure(figsize=(10,6)) # (10,6) för att den ska ha en tuple med två värden
+# plt.plot(df_import["Year"], df_import["Import"], marker="o")
+# plt.title("Yearly imported cars")
+# plt.xlabel("Year")
+# plt.ylabel("Imported cars")
+# plt.show()
 
 #   e) Draw a line chart of all years and months for average CO2 emissions
 
@@ -51,14 +53,37 @@ plt.show()
 
 #   f) Draw a line chart of all years and months for electric cars import where it's relevant.
 
-df_electric = df.groupby("Year")["Import_Electric"]
+df_electric = df.dropna(subset=["Import_Electric"])
+
+# Skapa en ny datekolumen med år och månad som strings
+
+# df_electric["Date"] = pd.to_datetime(df_electric["Year"].astype(str) + "-" + df_electric["Month"].astype(str))
+
+# plt.figure(figsize=(12,6))
+# plt.plot(df_electric["Date"], df_electric["Import_Electric"], marker="o")
+# plt.title("Electric Cars Import per Month")
+# plt.xlabel("Date")
+# plt.ylabel("Imported Electric Cars")
+# plt.grid(True, axis="y")
+# plt.tight_layout()
+# plt.show()
+
+# print(df_electric)
+
 
 #   g) Draw a line chart of average diesel share per year
 
+df_diesel_share = df.groupby("Year")["Diesel_Share"].mean().reset_index()
+print(df_diesel_share)
 
+plt.figure(figsize=(12,6))
+plt.plot(df_diesel_share["Year"], df_diesel_share["Diesel_Share"], marker="o")
+plt.title("Diesel share per year")
+plt.xlabel("Year")
+plt.ylabel("Diesel share in %")
+plt.show()
 
 #   h) Discuss some findings with a friend based on this dataset, and do plot more graphs
-
 
 
 
