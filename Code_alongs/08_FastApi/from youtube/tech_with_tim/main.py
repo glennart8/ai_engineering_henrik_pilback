f365b9f568b38b603256c203c7a693b5628d200b
glennart8 <henrikpilback@gmail.com>
2025-09-08 08:45:35 +0200

    Started a LLM/API project using ollama from tech_with_tim

---
+++ Code_alongs/08_FastApi/from youtube/tech_with_tim/main.py
@@ -0,0 +1,14 @@
+# ------- OLLAMA ---------
+# Länk: https://www.youtube.com/watch?v=cy6EAp4iNN4
+# Använder OLLAMA
+# skriv: 
+#   - ollama för info
+#   - ollama pull mistral för att hämta modellen
+#   - ollama run mistral för att köra modellen
+#   - /bye för att lämna
+
+# ------- PYCHARM --------
+# 
+
+
+
