3950cf6f23fa9a663be051e700a4a2704c31030b
glennart8 <henrikpilback@gmail.com>
2025-09-10 12:42:10 +0200

    Finished 08_FastAPI-CRUD

---
+++ Code_alongs/08_FastAPI_crud/data_processing.py
@@ -0,0 +1,43 @@
+from constants import DATA_PATH
+import json
+from pprint import pprint
+from pydantic import BaseModel, Field
+
+# LÄS IN DATA
+def read_json(filename: str):
+    with open(DATA_PATH / filename, 'r') as file:
+        data = json.load(file)
+    return data
+
+# PYDANTIC KLASSER
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    year: int = Field(gt = 1500, lt = 2026)
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 11,
+                "title": "Learn with AIgineer",
+                "author": "Kokchun Giang",
+                "year": 2025,
+            }
+        }
+    }
+
+class Library(BaseModel):
+    name: str
+    books: list[Book]    
+
+# Deserializar vår JSON-data in i en library-modell
+def library_data(filename):
+    json_data = read_json(filename)
+    return Library.model_validate(json_data)
+    
+
+if __name__ == '__main__':
+    
+    data = library_data("library.json")
+    pprint(data)
